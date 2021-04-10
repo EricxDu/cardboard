8c47ddc9f100e3f382ba7dc510a9c468df1ac2bf
Eric Duhamel <ericxdu23@gmail.com>
2021-04-10 01:33:19 -0700

    move cards around intuitively

--- cardboard.py
+++ cardboard.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 '''
 Copyright 2021 Eric Duhamel
 
@@ -15,47 +16,71 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
 
+import datetime
 import os
 import random
+import re
+import shutil
 import sys
 
 def main():
     path = os.getcwd()
     cardboard = Cardboard()
-    if len(sys.argv) < 3:
-        print("Usage: " + sys.argv[0] + " draft filename.m3u")
-        print("       " + sys.argv[0] + " draw filename.m3u")
-        print("       " + sys.argv[0] + " make .ext\n")
-        print("Note: make generates m3u files for all subfolders, so be careful")
+    list = ()
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+        splitted = filename.split(".")
+        if "deck" in splitted and "m3u" in splitted:
+            list = cardboard.get_deck_from_list(filename)
+            make_folder("DECK", list)
+        elif os.path.isdir(filename):
+            if filename.find("DECK") >= 0:
+                list = cardboard.get_hand(filename)
+                make_folder("HAND", list)
+            else:
+                list = cardboard.get_draft(filename)
+                make_folder("DRAFT", list)
     else:
-        filepath = os.path.join(path, sys.argv[2])
-        if sys.argv[1] == "draft":
-            list = cardboard.get_draft(filepath)
-        elif sys.argv[1] == "draw":
-            list = cardboard.get_draw(filepath)
-        elif sys.argv[1] == "make":
-            list = makelists(path, sys.argv[2])
-        for name in list:
-            print(name)
+        print("Please provide an m3u file or a folder containing card images")
+        print("I will create a deck for you from a .deck.m3u file")
+        print("I will move cards from a DECK folder to your HAND")
+        print("I will copy a draft of cards from any other folder")
 
-def makelists(path, ext):
-    list = []
-    for root, dirs, files in os.walk(path):
-        for i, set_name in enumerate(dirs):
-            set_path = os.path.join(path, set_name)
-            for root, dirs, files in os.walk(set_path):
-                set_filename = set_name.replace(" ", "") + ".m3u"
-                f = open(set_filename, "w")
-                for card_name in files:
-                    if card_name.endswith(ext):
-                        f.write(os.path.join(set_path, card_name) + "\n")
-                        list.append(card_name)
-    return list
+def make_folder(prefix, list):
+    isotime = datetime.datetime.now().replace(microsecond=0).isoformat()
+    folder = prefix + "".join(re.split("-|T|:", isotime))
+    os.mkdir(os.path.join(os.getcwd(), folder))
+    for i, path in enumerate(list):
+        splitted = os.path.split(path)
+        filename = splitted[len(splitted)-1]
+        if prefix == "HAND":
+            shutil.move(path, os.path.join(folder, filename))
+        else:
+            name, ext = filename.rsplit(".", 1)
+            filename = name + " (" + str(i+1) + ")" + "." + ext
+            shutil.copy(path, os.path.join(folder, filename))
+    print("Created " + folder)
 
+def make_list(filename, list):
+    f = open(filename, "w")
+    for path in list:
+        f.write(path + "\n")
 
 class Cardboard():
     def __init__(self):
         return None
+
+    def list(self, filepath):
+        f = open(filepath, "r")
+        set_list = []
+        for filepath in f.readlines():
+            set_list.append(filepath.split("\n")[0])
+        return set_list
+
+    def get_deck_from_list(self, path):
+        ''' Take a decklist and return all the cards.'''
+        list = self.list(path)
+        return list
 
     def get_draft(self, filepath, spread="UUUURCCCCCCCCCC"):
         ''' Take a setlist and open pack of booster cards.
@@ -68,7 +93,7 @@ class Cardboard():
             draft_list.append(random.choice(set_list))
         return draft_list
 
-    def get_draw(self, filepath, many=7):
+    def get_hand(self, filepath, many=7):
         ''' Take a decklist and draw a hand of cards from it.
 
             This method can only return one of each card in a list
@@ -79,12 +104,16 @@ class Cardboard():
             draft_list.append(random.choice(set_list))
         return draft_list
 
-    def get_list(self, filepath):
-        f = open(filepath, "r")
-        set_list = []
-        for filepath in f.readlines():
-            set_list.append(filepath.split("\n")[0])
-        return set_list
+    def get_list(self, path, ext="gif png"):
+        extens = ext.split(" ")
+        if os.path.isdir(path):
+            list = []
+            for root, dirs, files in os.walk(path):
+                for name in files:
+                    splitted = name.split(".")
+                    if splitted[len(splitted)-1] in extens:
+                        list.append(os.path.join(path, name))
+        return list
 
 
 if __name__ == "__main__":
