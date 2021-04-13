8d472b2de2981b7d7b997096e3c1f5b695c460d6
Eric Duhamel <ericxdu23@gmail.com>
2021-04-12 17:13:12 -0700

    refined the .deck .draft .set system

--- cardboard.py
+++ cardboard.py
@@ -32,42 +32,47 @@ def main():
         splitted = filename.split(".")
         if "deck" in splitted and "m3u" in splitted:
             print("Constructing a deck from " + filename)
-            list = cardboard.get_deck_from_list(filename)
-            make_folder("DECK", list)
+            list = cardboard.list_deck(filename)
+            copy_cards(list, ".deck")
+        elif "set" in splitted and "m3u" in splitted:
+            print("Grabbing 15 boosters from " + filename)
+            list = cardboard.list_draft(filename)
+            copy_cards(list, ".draft")
         elif os.path.isdir(filename):
-            if filename.find("DECK") >= 0:
-                print("Drawing cards from " + filename)
+            if "deck" in splitted:
+                print("Drawing 7 cards from " + filename)
                 list = cardboard.get_hand(filename)
-                make_folder("HAND", list)
-            else:
-                print("Grabbing a booster from " + filename)
-                list = cardboard.get_draft(filename)
-                make_folder("DRAFT", list)
+                move_cards(list, "YOURHAND")
     else:
+        print("Invoked: " + sys.argv[0])
         print("Please provide an m3u file or a folder containing card images")
-        print("I will create a DECK for you from a .deck.m3u file")
-        print("I will move cards from a DECK folder to your HAND")
-        print("I will copy cards from a set folder to create a DRAFT")
+        print("I will create a .deck folder from a .deck.m3u file")
+        print("I will create a .draft folder from a .set.m3u file")
+        print("I will move cards from a .deck folder to YOURHAND")
 
-def make_folder(folder, list):
+def copy_cards(list, ext):
     isotime = datetime.datetime.now().replace(microsecond=0).isoformat()
     stamp = "".join(re.split("-|T|:", isotime))
-    if folder == "DECK" or folder == "DRAFT":
-        folder = folder + stamp
-        print("creating " + folder)
-    else:
-        print("moving " + str(len(list)) + " cards to " + folder)
-    if not os.path.isdir(os.path.join(os.getcwd(), folder)):
-        os.mkdir(os.path.join(os.getcwd(), folder))
+    fname = stamp + ext
+    if not os.path.isdir(os.path.join(os.getcwd(), fname)):
+        print("cardboard: creating folder " + fname)
+        os.mkdir(os.path.join(os.getcwd(), fname))
+    print("cardboard: copying " + str(len(list)) + " cards to " + fname)
     for i, path in enumerate(list):
-        splitted = os.path.split(path)
-        filename = splitted[len(splitted)-1]
-        if folder == "HAND":
-            shutil.move(path, os.path.join(folder, filename))
-        else:
-            name, ext = filename.rsplit(".", 1)
-            filename = name + " (" + str(i+1) + ")" + "." + ext
-            shutil.copy(path, os.path.join(folder, filename))
+        filename = os.path.basename(path)
+        name, ext = filename.rsplit(".", 1)
+        filename = name + " (" + str(i+1) + ")" + "." + ext
+        # should the following paths be absolutized?
+        shutil.copy(path, os.path.join(fname, filename))
+
+def move_cards(list, folder):
+    if not os.path.isdir(os.path.join(os.getcwd(), folder)):
+        print("cardboard: creating folder " + folder)
+        os.mkdir(os.path.join(os.getcwd(), folder))
+    print("cardboard: moving " + str(len(list)) + " cards to " + folder)
+    for i, path in enumerate(list):
+        filename = os.path.basename(path)
+        shutil.move(path, os.path.join(folder, filename))
 
 def make_list(filename, list):
     f = open(filename, "w")
@@ -78,27 +83,28 @@ class Cardboard():
     def __init__(self):
         return None
 
-    def list(self, filepath):
-        f = open(filepath, "r")
-        set_list = []
-        for filepath in f.readlines():
-            set_list.append(filepath.split("\n")[0])
-        return set_list
+    def list(self, path):
+        f = open(path, "r")
+        list = []
+        for path in f.readlines():
+            name = path.split("\n")[0]
+            list.append(name)
+        return list
 
-    def get_deck_from_list(self, path):
+    def list_deck(self, path):
         ''' Take a decklist and return all the cards.'''
         list = self.list(path)
         return list
 
-    def get_draft(self, filepath, spread="UUUURCCCCCCCCCC"):
+    def list_draft(self, path, spread="UUUURCCCCCCCCCC"):
         ''' Take a setlist and open pack of booster cards.
 
             This method can draw multiples of the same card from a list
             because it is drafting from a randomized booster pack.'''
-        set_list = self.get_list(filepath)
+        list = self.list(path)
         draft_list = []
         for i, rarity in enumerate(spread):
-            draft_list.append(random.choice(set_list))
+            draft_list.append(random.choice(list))
         return draft_list
 
     def get_hand(self, filepath, many=7):
@@ -116,8 +122,8 @@ class Cardboard():
 
     def get_list(self, path, ext="gif png"):
         extens = ext.split(" ")
+        list = []
         if os.path.isdir(path):
-            list = []
             for root, dirs, files in os.walk(path):
                 for name in files:
                     splitted = name.split(".")
@@ -128,4 +134,4 @@ class Cardboard():
 
 if __name__ == "__main__":
     main()
-    quit()
+    sys.exit()
